09b0ab6f0e6c633cd298ffcb8b62e17a23a989b6
hoseasandstrom <Lakyn4ever>
2016-05-26 11:27:59 -0400

    Add functionalty

--- src/com/studenttheironyard/Main.java
+++ src/com/studenttheironyard/Main.java
@@ -1,8 +1,43 @@
 package com.studenttheironyard;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws FileNotFoundException {
+        ArrayList<Post> posts = new ArrayList<>();
+
+        //parse file
+        File f = new File("posts.txt");
+        Scanner fileScanner = new Scanner(f);
+        while (fileScanner.hasNext()) {
+            String line = fileScanner.nextLine();
+            String[] columns = line.split("\\|");
+            Post post = new Post(Integer.valueOf(columns[0]), columns[1], columns[2]);
+            posts.add(post);
+        }
+
+        //start infinite loop
+        Scanner consoleScanner = new Scanner(System.in);
+        int currentPost = -1;
+        while (true) {
+            //print out replies to current post
+            int postId = 0;
+            for (Post post : posts) {
+                if (post.replyId == currentPost) {
+                    System.out.printf("[%s] %s by %s\n",postId, post.text, post.author);
+                }
+                postId++;
+            }
+            //ask for new id
+            System.out.println("Type the id you want to see replies to:");
+            currentPost = Integer.valueOf(consoleScanner.nextLine());
+
+        }
     }
 }
+//always start with the data
+//how are we going to store it
